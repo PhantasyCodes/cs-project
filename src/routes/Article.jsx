bdfeb704b7aec1ffb911ab638430cf8f07f2e249
David King <davidkingmwai@gmail.com>
2023-04-27 22:30:53 +0300

    Update

--- src/routes/Article.jsx
+++ src/routes/Article.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment } from '@fortawesome/free-solid-svg-icons'
 
 import './Article.css'
 
@@ -12,6 +14,14 @@ const Article = () => {
         <div className="article-holder">
             <h2 className='genre'>HEALTH</h2>
             <h1 className='title'>The Benefits of Hiking for Mental Health</h1>
+            <h3 className='author'>by: John Doe</h3>
+            <h3 className='date'>Updated April 20, 2023 at 9.19 p.m | Published April 19, 2023 at 9.00 a.m</h3>
+            <div className="article-img"></div>
+            <div className="article-links">
+                <div className="article-link">
+                <FontAwesomeIcon icon={faComment} />
+                                                </div>
+            </div>
         </div>
     </div>
   )
