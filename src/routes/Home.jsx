1dc4fd4d72214c9abc2be33562475681faa274fb
David King <davidkingmwai@gmail.com>
2023-04-27 21:33:35 +0300

    Update

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,10 +3,20 @@ import Navbar from '../components/Navbar'
 
 import './Home.css'
 
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>APRIL 22, 2023</h2>
+      <h1>The Student's Digest</h1>
+    </div>
+  )
+}
+
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
