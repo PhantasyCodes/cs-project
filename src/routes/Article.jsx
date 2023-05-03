573a559a27b34af36d58a29be715f934abbabac3
David King <davidkingmwai@gmail.com>
2023-05-03 16:53:43 +0300

    Update

--- src/routes/Article.jsx
+++ src/routes/Article.jsx
@@ -19,8 +19,25 @@ const Article = () => {
             <div className="article-img"></div>
             <div className="article-links">
                 <div className="article-link">
-                <FontAwesomeIcon icon={faComment} />
-                                                </div>
+                    <FontAwesomeIcon icon={faComment} />
+                    Comment
+                </div>
+            </div>
+            <div className="article-body">
+                <p>
+                    Hiking is not only an enjoyable way to explore the great outdoors, but it also offers numerous benefits for mental health. 
+                    Whether you're looking to improve your mood, reduce stress, or simply disconnect from the daily grind, hiking can be a great way to achieve these goals. 
+                    Here are some of the benefits of hiking for mental health. <br></br><br></br> Hiking is not only good for your physical health but also your mental health. 
+                    Spending time in nature has been shown to reduce stress, anxiety, and depression. 
+                    Being surrounded by the natural beauty of forests, mountains, and rivers can be a calming and relaxing experience. 
+                    Hiking also provides a sense of achievement and accomplishment that can boost self-esteem and confidence.<br></br><br></br>
+                    Moreover, hiking can help you to disconnect from the stress of daily life and reconnect with yourself. 
+                    In a world where we are constantly bombarded with digital distractions, hiking allows us to unplug and enjoy the present moment. 
+                    It can be a great way to clear your mind, reflect on your thoughts, and gain a fresh perspective. <br></br><br></br>
+                    Hiking is an activity that can be enjoyed by people of all ages and fitness levels. 
+                    However, it is important to start slowly and build up your endurance and strength gradually. 
+                    You can start by choosing an easy trail that is suitable for beginners and gradually progress to more challenging trails as you gain experience and confidence.
+                </p>
             </div>
         </div>
     </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,69 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
+import { csv, scaleLinear, max, scaleBand } from 'd3'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 
 import './Home.css'
 
+const csvurl = "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv"
+
+const width = 960
+const height = 500
+const margin = { top: 20, right: 20, bottom: 20, left: 20 }
+
 
 const Home = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const row = (d) => {
+      d.Population = +d['2020'];
+      return d;
+    };
+    csv(csvurl, row).then((data) => {
+      setData(data.slice(0, 10));
+    });
+  }, [])
+
+  const innerHeight = height - margin.top - margin.bottom
+  const innerWidth = width - margin.left - margin.right
+
+  const yScale = data && scaleBand()
+    .domain(data.map((d) => d.Country))
+    .range([0, innerHeight]);
+
+  const xScale = data && scaleLinear()
+    .domain([0, max(data, (d) => d.Population)])
+    .range([0, innerWidth]);
+
   return (
     <>
       <Navbar />
       <Header />
+      <div className="home">
+        <svg width={width} height={height}>
+          <g transform={`translate(${margin.left},${margin.top})`}>
+            {xScale.ticks().map(tickValue => (
+              <line 
+                x1={xScale(tickValue)} 
+                y1={0}
+                x2={xScale(tickValue)} 
+                y2={innerHeight} 
+                stroke="black"
+              />
+            ))}
+            {data && data.map((d) => (
+              <rect
+                key={d.Country}
+                y={yScale(d.Country)}
+                width={xScale(d.Population)}
+                height={yScale.bandwidth()}
+              />
+            ))}
+          </g>
+        </svg>
+        
+      </div>
     </>
   )
 }
